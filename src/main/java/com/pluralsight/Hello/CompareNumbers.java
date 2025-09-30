a223c9a7ebc2eae2eaf5b7fd7ce724687488ae72
Mario-Code7 <marioroa71@yahoo.com>
2025-09-30 09:19:11 -0500

    exercise 1-4 completed and refracted

--- src/main/java/com/pluralsight/Hello/CompareNumbers.java
+++ src/main/java/com/pluralsight/Hello/CompareNumbers.java
@@ -3,16 +3,15 @@ package com.pluralsight.Hello;
 import java.util.Scanner;
 
 public class CompareNumbers {
-    static Scanner scanner = new Scanner(System.in);
+   static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        System.out.println(isEven(5));
+    }
     public static boolean isEven(int number) {
         return number % 2 == 0;
     }
     public static boolean isPositive(double number) {
         return number > 0;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isEven(7));
     }
 }
